caaad9ae05faab3cdea3eb8d2bf067cfadca83cf
Filip Malm-Bägén <Filip.bagen@gmail.com>
2019-01-29 18:25:52 +0100

    Removed code

--- watch-master/assets/js/script.js
+++ watch-master/assets/js/script.js
@@ -1,63 +1,37 @@
-//clock
-function clock() {
-    const fullDate = new Date();
-    let hours = fullDate.getHours();
-    let minutes = fullDate.getMinutes();
+// clock
+function clock () {
+  const fullDate = new Date()
+  let hours = fullDate.getHours()
+  let minutes = fullDate.getMinutes()
 
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
+  minutes = ('0'+minutes).slice(-2)
+  hours = ('0'+hours).slice(-2)
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    document.querySelector('#hours').innerHTML = hours;
-    document.querySelector('#minutes').innerHTML = minutes;
+  document.querySelector('#hours').innerHTML = hours
+  document.querySelector('#minutes').innerHTML = minutes
 }
 
-setInterval(clock, 1000);
-
-
+setInterval(clock, 1000)
 
 // get weekday and day
-function currentDate() {
-    const fullDate = new Date();
-    let weekday = new Array(7);
-    weekday[0] = "SUM";
-    weekday[1] = "MON";
-    weekday[2] = "TUE";
-    weekday[3] = "WED";
-    weekday[4] = "THU";
-    weekday[5] = "FRI";
-    weekday[6] = "SAT";
+function currentDate () {
+  const today = new Date()
+  const dayNumber = today.getDate()
 
-    let n = weekday[fullDate.getDay()];
-    let day = fullDate.getDate();
-
-    document.querySelector('#day').innerHTML = day;
-    document.querySelector('#weekday').innerHTML = n;
+  document.querySelector('#day').innerHTML = dayNumber
+  document.querySelector('#weekday').innerHTML = getDayString()
 }
 
-setInterval(currentDate);
+setInterval(currentDate)
 
-
-function start() {
-    clock();
-    currentDate();
-    myFunction()
+function init () {
+  clock()
+  currentDate()
 }
 
-
-
-
-
-// function goodnight() {
-//     document.getElementById("on").style.display = "none";
-//     document.getElementById("off").style.display = "inline";
-// }
-
-// function eveningLights() {
-//     //  document.getElementById("off").style.display = "none";
-//     document.getElementById("on").style.display = "inline";
-// }
+function getDayString () {
+  const today = new Date()
+  const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+  const dayString = weekdays[today.getDay()]
+  return dayString
+}
